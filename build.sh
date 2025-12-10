900f59373ecd9e96f39ef0783954eb3dd3b4659b
Jair Letelier <jairletelier23@gmail.com>
2025-12-10 05:17:33 -0300

    Agrega script para la creación automática de superusuario en Render

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Intenta imprimir una línea y luego ejecutar el script de forma independiente
-echo "--- INICIANDO PRUEBA DE ADMIN SCRIPT ---"
-python create_admin_on_startup.py
-
-# Luego ejecuta los comandos normales
+# 1. INSTALACIÓN (Necesaria para los pasos 3 y 4)
 pip install -r requirements.txt
+
+# 2. ESTÁTICOS (Necesaria para que la página de login tenga estilos)
 python manage.py collectstatic --no-input
+
+# 3. MIGRACIONES (Necesaria para que las tablas de usuario existan)
 python manage.py migrate --no-input
+
+# 4. CREACIÓN DEL SUPERUSUARIO (Debe ser el último)
+python create_admin_on_startup.py
