5a4a42fc9c98dc7aadd79c162b5d05ff397d79e2
Jair Letelier <jairletelier23@gmail.com>
2025-12-10 05:08:05 -0300

    Agrega script para la creación automática de superusuario en Render

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-# 1. Instala dependencias
 pip install -r requirements.txt
-
-# 2. Recopila estáticos
 python manage.py collectstatic --no-input
-
-# 3. Migra la base de datos (CREA LAS TABLAS)
 python manage.py migrate --no-input
-
-# 4. CREAR SUPERUSUARIO CON VARIABLES DE ENTORNO (Reemplaza el loaddata)
 python create_admin_on_startup.py
