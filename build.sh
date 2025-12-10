a4282fbe55e5cc1305080f29395959b7e1ead6d7
Jair Letelier <jairletelier23@gmail.com>
2025-12-10 05:15:06 -0300

    Agrega script para la creación automática de superusuario en Render

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# Intenta imprimir una línea y luego ejecutar el script de forma independiente
+echo "--- INICIANDO PRUEBA DE ADMIN SCRIPT ---"
+python create_admin_on_startup.py
+
+# Luego ejecuta los comandos normales
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
-python create_admin_on_startup.py
